9af86d4f9e0691c4987cd2dac000d8ec6e2fb3fb
Elias <e.mueller24@gmx.de>
2021-02-26 09:53:44 +0100

    Initial commit

---
+++ src/main/java/dhbw/vs/uebungsblatt2service/Uebungsblatt2serviceApplication.java
@@ -0,0 +1,13 @@
+package dhbw.vs.uebungsblatt2service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uebungsblatt2serviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Uebungsblatt2serviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dhbw/vs/uebungsblatt2service/Uebungsblatt2serviceApplicationTests.java
@@ -0,0 +1,13 @@
+package dhbw.vs.uebungsblatt2service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uebungsblatt2serviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
