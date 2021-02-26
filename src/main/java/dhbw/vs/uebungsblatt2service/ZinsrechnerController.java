8b82277b8379a0d660e461d28ff442e274a1f408
Elias <e.mueller24@gmx.de>
2021-02-26 12:03:50 +0100

    Funktion für Rückzahlungsplan

--- src/main/java/dhbw/vs/uebungsblatt2service/ZinsrechnerController.java
+++ src/main/java/dhbw/vs/uebungsblatt2service/ZinsrechnerController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
 @RequestMapping("/zinsen")
 public class ZinsrechnerController {
@@ -16,6 +18,13 @@ public class ZinsrechnerController {
         return laufzeit(kreditbetrag, zinssatz, rueckzahlung);
     }
 
+    @GetMapping
+    public Collection<Double> rueckzahlungsplan(@RequestParam double kreditbetrag,
+                                                @RequestParam double zinssatz,
+                                                @RequestParam double rueckzahlung) {
+        return plan(kreditbetrag, zinssatz, rueckzahlung);
+    }
+
     public int laufzeit(double kreditbetrag, double zinssatz, double rueckzahlung) {
         zinssatz = zinssatz / 100;
         double t = ((rueckzahlung * 12) / kreditbetrag) - zinssatz;
@@ -25,5 +34,18 @@ public class ZinsrechnerController {
         int laufzeit = (int) Math.ceil(ergebnis);
 
         return laufzeit;
+    }
+
+    public Collection<Double> plan(double kreditbetrag, double zinssatz, double rueckzahlung) {
+        Collection<Double> restbetraege = null;
+        double q = 1 + (zinssatz / 12);
+        int laufzeit = laufzeit(kreditbetrag, zinssatz, rueckzahlung);
+        restbetraege.add(kreditbetrag);
+        for (int i = 1; i < laufzeit; i++) {
+            double restschuld = kreditbetrag * Math.pow(q, i) - rueckzahlung * ((Math.pow(q, i) - 1) / (zinssatz / 12));
+            restbetraege.add(restschuld);
+        }
+        restbetraege.add(0.0);
+        return restbetraege;
     }
 }
